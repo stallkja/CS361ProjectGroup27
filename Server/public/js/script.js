d5cb4644de9e89c370eb08f25eb26607991aa3a7
ferchlam <ferchlam@users.noreply.github.com>
2019-03-09 11:37:38 -0800

    applyformarket
    This is the changes to apply for a market user account.

--- Server/public/js/script.js
+++ Server/public/js/script.js
@@ -16,6 +16,39 @@ function login(){
    }});
 
    req.send(
-      "username=" + user + "&password=" + pass
+      "username=" + user + "&password=" + pass + "&accountType=users"
    );
+}
+
+
+function newMarket()
+{
+   const user = document.getElementById('marketName').value;
+   const pass = document.getElementById('password').value;
+   const email = document.getElementById('email').value;
+   const phone = document.getElementById('phone').value;
+   const contact = document.getElementById('contact').value;
+   const address = document.getElementById('address').value;
+   const city = document.getElementById('city').value;
+   const state = document.getElementById('state').value;
+   const zip = document.getElementById('zip').value;
+
+   let req = new XMLHttpRequest();
+
+   req.open('POST', 'createMarket', true);
+   req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+   req.addEventListener('load',function(){
+      if(req.status >= 200 && req.status < 400){
+      //   window.sessionStorage.token = JSON.parse(req.response).jwt;
+         //window.location.href = "/home";
+	 alert("Success");
+      } else {
+         console.log('Error');
+         alert("Invalid Input.");
+   }});
+
+   req.send(
+      "marketName=" + user + "&password=" + pass + "&email=" + email + "&phone=" + phone + "&contact=" + contact + "&address=" + address + "&city=" + city + "&state=" + state + "&zip=" + zip
+   );
+
 }
