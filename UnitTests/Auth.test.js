e2e4fafdeb840853d2dcef399d2072e279fb15b9
James Pham <vnlegend@gmail.com>
2019-03-08 18:03:49 -0800

    Added error handling for body parameters

--- UnitTests/Auth.test.js
+++ UnitTests/Auth.test.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const route = "http://157.230.158.143/authApp";
+const route = "http://157.230.158.143/auth";
 
 console.log("connecting to " + route);
 
@@ -21,27 +21,37 @@ var wrongUsername = {
 
 function printLog(passed, msg) {
     if (passed) {
-        console.log("\x1b[32mPASS:\x1b[0m" + msg);
+        console.log("\x1b[32mPASS:\x1b[0m " + msg);
     } else {
-        console.log("\x1b[31mFAIL:\x1b[0m" + msg);
+        console.log("\x1b[31mFAIL:\x1b[0m " + msg);
     }
 }
 
+function assertTrue(a, b, msg) {
+    if(a === b) {
+        printLog(1, msg);
+    }
+    else {
+        printLog(0, msg);
+    }
+}
 
 // Test username password login
-axios.post(route, validUser).then(result => {
-    console.log('Testing valid user');
-    console.log(result.data);
+axios.post(route, validUser).then(response => {
+    assertTrue(response.data.auth, 'Valid', 'Log in with valid username and password combo should return Valid');
 });
 
-axios.post(route, wrongPassword).then(result => {
-    console.log('Testing wrong password');
-    console.log(result.data);
+axios.post(route, wrongPassword).then(response => {
+}).catch(error => {
+    assertTrue(error.response.data.auth, 'Invalid', 'Log in with valid username and wrong password should return Invalid');
 });
 
-axios.post(route, wrongUsername).then(result => {
-    console.log('Testing wrong username');
-    console.log(result.data);
+axios.post(route, wrongUsername).then(response => {
+}).catch(error => {
+    assertTrue(error.response.data.auth, 'Not Found', 'Log in with username that does not exist should return Not Found');
 });
 
-
+axios.post(route, {}).then(response => {
+}).catch(error => {
+    assertTrue(error.response.data.auth, 'Bad parameters', 'Log in without any username or password should return Bad Parameters');
+});
